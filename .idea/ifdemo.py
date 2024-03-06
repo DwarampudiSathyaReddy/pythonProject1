25ee7741ed18a4f6ced237315fb13936bfbd2995
dwarampudi Sathya Reddy <sathyareddy1122005@gmail.com>
2024-03-06 08:03:47 +0530

    initial commit

---
+++ .idea/ifdemo.py
@@ -0,0 +1,8 @@
+principal=int(input("Enter the Principle Amount"))
+rate =float(input("rate of intrest"))
+time=int(input("time period"))
+ci=(principal*rate*time)/100
+print("Compound intrest=",ci
+
+
+      333)
---
+++ ListMethods.py
@@ -0,0 +1,40 @@
+'''
+List methods:
+1.start()
+2.clear()
+3.append()
+4.count()
+5.extend()
+6.insert()
+7.index()
+8.len()
+9.remove()
+10.pop()
+11.reverse()
+12.copy()
+13.len()
+14.min()
+15.max()
+16.del()
+'''
+mylist=[1,2,3,4,55,6]
+mylist.sort()
+print(mylist)
+mylist.append(23)
+print(mylist)
+mylist.append([44,34,42,41])
+print(mylist)
+mylist.extend([25,78,98])
+print(mylist)
+print(mylist.count(4))
+maximum = max(mylist)
+print(max)
+minimum = min(mylist)
+print(min)
+del mylist[2]
+print(mylist)
+copylist = mylist.copy()
+print
+
+
+
