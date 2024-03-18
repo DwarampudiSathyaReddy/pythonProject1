603b3e06a39996a871e6ff89bbd4dffc27e3c360
dwarampudi Sathya Reddy <sathyareddy1122005@gmail.com>
2024-03-18 14:48:58 +0530

    initial commit

--- simple intrest.py
+++ simple intrest.py
@@ -1,10 +1,30 @@
-# Input
-principal = float(input("Enter the principal amount: "))
-rate = float(input("Enter the annual interest rate (%): "))
-time = float(input("Enter the time period (in years): "))
+from tkinter import *
+from tkinter import messagebox
+from PIL import ImageTk, Image
+
+def calculate_bill():
+    units =float(unitEntry.get())
+    rate_per_unit = 5.0
+    fixed_charge = 10.0
+    total_cost = (units * rate_per_unit) + fixed_charge
+    messagebox.showinfo("TOTAL COST  : ", f" ({total_cost})")
 
 
-simple_interest = (principal * rate * time) / 100
+window = Tk()
+window.title("GENERATE ELECTRICITY BILL")
+blabel = Label(window)
+blabel.grid()
+frame = Frame(window)
+frame.place(x=600,y=100)
 
-# Output
-print("Simple Interest: $", simple_interest)
+unit = Label(frame, text="Enter  the number of units consumed: ", font=('Arial', 15, 'bold'), bg='white', fg='firebrick')
+unit.grid(row=0, column=0, padx=10, pady=5)  # Add padx and pady to create space
+
+unitEntry = Entry(frame, width=30)
+unitEntry.grid(row=0, column=1)
+
+
+submit_button = Button(frame, text="Submit", command=calculate_bill)
+submit_button.grid(row=2, column=1)
+
+window.mainloop()
--- vote eligiblity.py
+++ vote eligiblity.py
@@ -1,14 +1,41 @@
-def check_voting_eligibility(age):
-    if age >= 18:
-        return True
+from tkinter import *
+from tkinter import messagebox
+from PIL import ImageTk, Image
+
+def check_eligibility():
+    num = int(numEntry.get())
+    if num >18:
+        messagebox.showinfo("ELIGIBLE OR NOT ", " Eligible to vote")
     else:
-        return False
+        messagebox.showinfo("ELIGIBLE OR NOT", "Not Eligible to vote")
 
-# Input age
-age = int(input("Enter your age: "))
 
-# Check if eligible to vote
-if check_voting_eligibility(age):
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote yet.")
+
+def even_odd():
+    num =int(numEntry.get())
+    if num % 2 == 0:
+        messagebox.showinfo("EVEN-ODD CHECKER"," It is even number")
+    else:
+        messagebox.showinfo("EVEN-ODD CHECKER"," It is odd number")
+
+window = Tk()
+window.title("Eligible to vote or not")
+blabel = Label(window)
+blabel.grid()
+frame = Frame(window)
+frame.place(x=600,y=100)
+
+num = Label(frame, text="Enter  the number : ", font=('Arial', 15, 'bold'), bg='white', fg='firebrick')
+num.grid(row=0, column=0, padx=10, pady=5)  # Add padx and pady to create space
+
+numEntry = Entry(frame, width=30)
+numEntry.grid(row=0, column=1)
+
+
+submit_button = Button(frame, text="Submit", command=check_eligibility)
+submit_button.grid(row=1, column=1)
+
+window.mainloop()
+
+
+
