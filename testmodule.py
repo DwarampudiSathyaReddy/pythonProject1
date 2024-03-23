4c828d68e292020d9eca783441e46e591d1ef47b
dwarampudi Sathya Reddy <sathyareddy1122005@gmail.com>
2024-03-23 12:39:58 +0530

    initial commit

---
+++ testmodule.py
@@ -0,0 +1,12 @@
+import Arithmetic
+from Arithmetic import *
+from factorial import *
+print(Arithmetic.add(10,20))
+print(Arithmetic.sub(20,40))
+print(Arithmetic.mul(22,30))
+print(add(10,10))
+print(sub(56,45))
+print(mul(2,3))
+
+x=factorial(5)
+print("Factorial of",x)
